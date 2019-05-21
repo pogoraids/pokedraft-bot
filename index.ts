03a754ec5436e01f110e7b674766d358af5ab49e
Mathias Rodriguez <mathias_rodriguez@transcanada.com>
2019-05-21 17:57:16 -0300

    feat: add local .env file for config, add managed guilds

--- index.ts
+++ index.ts
@@ -6,30 +6,48 @@ import { GREETING } from "./src/constants/messages";
 import { Listener } from "./src/modules/listener";
 import { Channels } from "./src/modules/channels";
 import { Configuration } from "./src/modules/configuration";
+import { ENV } from "./src/config";
 
 export class Index {
   constructor() {
     const client = new Discord.Client();
-    const TOKEN = readFileSync("./src/config/token");
-    let db = null;
+    const TOKEN = ENV.DISCORD_TOKEN;
 
     client.on("ready", () => {
       console.log(GREETING);
 
-      client.guilds.forEach(guild => {
-        // ToDo(): refactor in a helper + BotDBWrapper
-        sql
-          .open(resolve(dirname(".") + `/database/${guild.id}.sqlite`))
-          .then((database: sql.Database) => {
-            new Channels().createBotChannel(guild, client);
-          })
-          .catch(reason => {
-            console.error(
-              "Failed to retrieve database file / generic error\n",
-              reason
-            );
-          });
-      });
+      if (!!ENV.MANAGED_GUILD_ID) {
+        const guild = client.guilds.find(guild => guild.id === ENV.MANAGED_GUILD_ID);
+
+        if (!!guild) {
+          sql
+            .open(resolve(dirname(".") + `${ENV.SQLITE_FILEDIR}/${guild.id}.sqlite`))
+            .then((database: sql.Database) => {
+              new Channels().createBotChannel(guild, client);
+            })
+            .catch(reason => {
+              console.error(
+                "Failed to retrieve database file / generic error\n",
+                reason
+              );
+            });
+        }
+      } else {
+        client.guilds.forEach(guild => {
+          // ToDo(): refactor in a helper + BotDBWrapper
+          sql
+            .open(resolve(dirname(".") + `/database/${guild.id}.sqlite`))
+            .then((database: sql.Database) => {
+              new Channels().createBotChannel(guild, client);
+            })
+            .catch(reason => {
+              console.error(
+                "Failed to retrieve database file / generic error\n",
+                reason
+              );
+            });
+        });
+      }
     });
 
     client.on("message", new Listener().parse);
---
+++ src/config/index.ts
@@ -0,0 +1,18 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const ENV = {
+    PORT: process.env.PORT || '3333',
+
+    SQLITE_FILEDIR: process.env.SQLITE_FILEDIR || './database',
+    DISCORD_TOKEN: process.env.DISCORD_TOKEN || '',
+    MANAGED_GUILD_ID: process.env.MANAGED_GUILD_ID || '',
+
+    DB_HOST: process.env.DB_HOST || '127.0.0.1',
+    DB_PORT: process.env.DB_PORT || '3000',
+    DB_NAME: process.env.DB_NAME || 'draft',
+    DB_USER: process.env.DB_USER || 'root',
+    DB_PASSWORD: process.env.DB_PASSWORD || 'root',
+    DB_DIALECT: process.env.DB_DIALECT || 'mysql'
+};
